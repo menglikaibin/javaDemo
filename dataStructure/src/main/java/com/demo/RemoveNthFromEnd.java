c1a76dde7bf2574e7a6a68026529e861d9532393
wukaibin <429148925@qq.com>
2024-03-26 11:04:44 +0800

    移除链表倒数第n项

---
+++ dataStructure/src/main/java/com/demo/RemoveNthFromEnd.java
@@ -0,0 +1,59 @@
+package com.demo;
+
+import netscape.javascript.JSObject;
+
+/**
+ * @author 吴凯斌
+ * 创建时间 2024/3/26 10:27
+ */
+public class RemoveNthFromEnd {
+
+    public static ListNode removeNthFromEnd(ListNode head, int n)
+    {
+        ListNode total = head;
+        int size = 0;
+        while (total != null) {
+            size ++;
+            total = total.next;
+        }
+
+        int index = size - n;
+
+        ListNode result = new ListNode(0);
+
+        ListNode current = result;
+        int cur = 0;
+        while (head != null) {
+            if (cur == index) {
+                cur ++;
+                head = head.next;
+                continue;
+            }
+            cur ++;
+            current.next = new ListNode(head.val);
+
+            current = current.next;
+
+            head = head.next;
+        }
+
+        return result.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode fromEnd = removeNthFromEnd(listNode, 2);
+        while (fromEnd != null) {
+            System.out.println(fromEnd.val);
+            fromEnd = fromEnd.next;
+        }
+    }
+}
