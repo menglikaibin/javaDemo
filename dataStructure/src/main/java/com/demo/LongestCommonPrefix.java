42d04ba3c37f5174fde50ee75d1e456283a91a86
wukaibin <429148925@qq.com>
2024-03-26 10:08:43 +0800

    最长公共前缀第二解法

--- dataStructure/src/main/java/com/demo/LongestCommonPrefix.java
+++ dataStructure/src/main/java/com/demo/LongestCommonPrefix.java
@@ -35,9 +35,29 @@ public class LongestCommonPrefix {
         return prefix;
     }
 
+    public static String longestCommonPrefix2(String[] strs)
+    {
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
     public static void main(String[] args) {
 
         System.out.println(longestCommonPrefix(new String[]{"flow", "flower", "flwwwwww"}));
+        System.out.println(longestCommonPrefix2(new String[]{"flow", "flower", "flwwwwww"}));
 
     }
 
