2c4da4b2ce0fbc7215f56d33a157b32bcc214761
wukaibin <429148925@qq.com>
2024-04-22 17:05:22 +0800

    三数之和

---
+++ dataStructure/src/main/java/com/demo/list/ThreeSum.java
@@ -0,0 +1,72 @@
+package com.demo.list;
+
+import java.util.*;
+
+/**
+ * 15. 三数之和
+ * 中等
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。
+ * 请你返回所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 解释：
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+ * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+ * 注意，输出的顺序和三元组的顺序并不重要。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0,1,1]
+ * 输出：[]
+ * 解释：唯一可能的三元组和不为 0 。
+ * <p>
+ * 示例 3：
+ * 输入：nums = [0,0,0]
+ * 输出：[[0,0,0]]
+ * 解释：唯一可能的三元组和为 0 。
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ */
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+
+        int length = nums.length;
+        if (length < 3) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < length - 2; i++) {
+            int left = i + 1;
+            int right = length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left += 1;
+                    right -= 1;
+                } else if (sum > 0) {
+                    right -= 1;
+                } else {
+                    left += 1;
+                }
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
