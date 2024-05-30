b4dc7d13eacc7484d218f40fad73be25ac754a0d
wukaibin <429148925@qq.com>
2024-05-30 20:24:40 +0800

    转轮数组

---
+++ dataStructure/src/main/java/com/demo/list/Rotate.java
@@ -0,0 +1,56 @@
+package com.demo.list;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ *
+ * 示例 2:
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
+ */
+public class Rotate {
+
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        if (length == 0) {
+            return;
+        }
+
+        if (k > length) {
+            k = k % length;
+        }
+
+        int[] ints = new int[length];
+        for (int i = 0; i < length; i++) {
+            if (k - i > 0) {
+                ints[i] = nums[length - k + i];
+            } else {
+                ints[i] = nums[i - k];
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            nums[i] = ints[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        Rotate rotate = new Rotate();
+        int[] nums = {-1, -100, 3, 99, 100, 120};
+        rotate.rotate(nums, 8);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
