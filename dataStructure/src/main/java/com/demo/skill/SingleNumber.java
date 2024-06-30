ae8a21155969394d6a2e00c60ba1f110f502f21b
menglikaibin <429148925@qq.com>
2024-06-30 23:33:19 +0800

    136,只出现一次的数字

---
+++ dataStructure/src/main/java/com/demo/skill/SingleNumber.java
@@ -0,0 +1,61 @@
+package com.demo.skill;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 136
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+ *
+ *
+ *
+ * 示例 1 ：
+ *
+ * 输入：nums = [2,2,1]
+ * 输出：1
+ * 示例 2 ：
+ *
+ * 输入：nums = [4,1,2,1,2]
+ * 输出：4
+ * 示例 3 ：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ *
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -3 * 104 <= nums[i] <= 3 * 104
+ * 除了某个元素只出现一次以外，其余每个元素均出现两次。
+ */
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length / 2 + 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                map.put(nums[i], 2);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        for (Integer key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber singleNumber = new SingleNumber();
+        System.out.println(singleNumber.singleNumber(new int[]{2, 2, 1}));
+    }
+
+}
