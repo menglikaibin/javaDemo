160ecda7fed60ace794549699b6622bb7f9d8629
wukaibin <429148925@qq.com>
2024-07-31 20:51:37 +0800

    404,左叶子之和

---
+++ dataStructure/src/main/java/com/demo/treenode/simple/SumOfLeftLeaves.java
@@ -0,0 +1,65 @@
+package com.demo.treenode.simple;
+
+import com.demo.treenode.TreeNode;
+
+/**
+ * 404,左叶子之和
+ *
+ * 给定二叉树的根节点 root ，返回所有左叶子之和。
+ *
+ * 示例 1：
+ * 输入: root = [3,9,20,null,null,15,7]
+ * 输出: 24
+ * 解释: 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
+ * 示例 2:
+ * 输入: root = [1]
+ * 输出: 0
+ * 提示:
+ *
+ * 节点数在 [1, 1000] 范围内
+ * -1000 <= Node.val <= 1000
+ * Related Topics
+ * 树
+ * 深度优先搜索
+ * 广度优先搜索
+ * 二叉树
+ */
+public class SumOfLeftLeaves {
+
+    int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        getLeftSum(root);
+
+        return sum;
+    }
+
+    private void getLeftSum(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.left != null) {
+            if (node.left.left == null && node.left.right == null) {
+                sum += node.left.val;
+            }
+        }
+        getLeftSum(node.left);
+        getLeftSum(node.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                        new TreeNode(15),
+                        new TreeNode(7)
+                )
+        );
+
+        SumOfLeftLeaves sumOfLeftLeaves = new SumOfLeftLeaves();
+        System.out.println(sumOfLeftLeaves.sumOfLeftLeaves(treeNode));
+    }
+
+}
