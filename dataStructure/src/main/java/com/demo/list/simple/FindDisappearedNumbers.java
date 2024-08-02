13f4c615a1f7e96375180e913d78be611fcddb3e
menglikaibin <429148925@qq.com>
2024-08-02 23:59:17 +0800

    445,找到所有数组消失的数字

---
+++ dataStructure/src/main/java/com/demo/list/simple/FindDisappearedNumbers.java
@@ -0,0 +1,47 @@
+package com.demo.list.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 445,找到所有数组消失的数字
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ *
+ * 示例 1：
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[5,6]
+ *
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[2]
+ *
+ * 提示：
+ *
+ * n == nums.length
+ * 1 <= n <= 105
+ * 1 <= nums[i] <= n
+ * 进阶：你能在不使用额外空间且时间复杂度为 O(n) 的情况下解决这个问题吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * Related Topics
+ * 数组
+ * 哈希表
+ */
+public class FindDisappearedNumbers {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> data = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            data.add(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums) {
+            if (!data.contains(num)) {
+                result.add(num);
+            }
+        }
+
+        return result;
+    }
+
+}
