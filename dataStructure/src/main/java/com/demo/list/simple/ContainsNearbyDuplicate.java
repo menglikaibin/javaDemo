5b19102d580ee115b6a66e392c6484d5b243e783
menglikaibin <429148925@qq.com>
2024-08-09 23:58:25 +0800

    219,存在重复元素

---
+++ dataStructure/src/main/java/com/demo/list/simple/ContainsNearbyDuplicate.java
@@ -0,0 +1,51 @@
+package com.demo.list.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 219,存在重复元素
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3,1], k = 3
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：nums = [1,0,1,1], k = 1
+ * 输出：true
+ * 示例 3：
+ *
+ * 输入：nums = [1,2,3,1,2,3], k = 2
+ * 输出：false
+ * 提示：
+ *
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * 0 <= k <= 105
+ * Related Topics
+ * 数组
+ * 哈希表
+ * 滑动窗口
+ */
+public class ContainsNearbyDuplicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (map.containsKey(num) && (i - map.get(num)) < k) {
+                return true;
+            }
+
+            map.put(num, i);
+        }
+
+        return false;
+    }
+
+}
