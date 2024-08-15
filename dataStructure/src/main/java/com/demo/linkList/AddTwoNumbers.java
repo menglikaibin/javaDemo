f1dda27a2f35290abc568e54afc6f9c611976348
menglikaibin <429148925@qq.com>
2024-08-15 23:59:42 +0800

    445,两数相加

---
+++ dataStructure/src/main/java/com/demo/linkList/AddTwoNumbers.java
@@ -0,0 +1,68 @@
+package com.demo.linkList;
+
+import java.util.Stack;
+
+/**
+ * 445,两数相加
+ * <p>
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * <p>
+ * 示例1：
+ * 输入：l1 = [7,2,4,3], l2 = [5,6,4]
+ * 输出：[7,8,0,7]
+ * <p>
+ * 示例2：
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[8,0,7]
+ * <p>
+ * 示例3：
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * 提示：
+ * <p>
+ * 链表的长度范围为 [1, 100]
+ * 0 <= node.val <= 9
+ * 输入数据保证链表代表的数字无前导 0
+ * 进阶：如果输入链表不能翻转该如何解决？
+ * <p>
+ * Related Topics
+ * 栈
+ * 链表
+ * 数学
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result = new ListNode(0);
+
+        Stack<Integer> stack1 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        Stack<Integer> stack2 = new Stack<>();
+        while (l2 != null) {
+            stack2.push(l2.val);
+
+            l2 = l2.next;
+        }
+
+        int carry = 0;
+        ListNode head = null;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry > 0) {
+            int sum = carry;
+            sum += stack1.isEmpty() ? 0 : stack1.pop();
+            sum += stack2.isEmpty() ? 0 : stack2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+        return head;
+    }
+
+}
